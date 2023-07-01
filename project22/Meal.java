361d0d1d000f2dec09789fe83c9b517e8751b81c
Meshkelizzz32 <meshkeliszzz@gmail.com>
2023-07-01 17:15:43 +0300

    Restaurant_With_Queues

---
+++ project22/Meal.java
@@ -0,0 +1,13 @@
+package project28;
+
+public class Meal {
+	  public static void main(String[] args) {
+	    for(int i = 0; i < 5; i++) {
+	      for(Course course : Course.values()) {
+	        Food food = course.randomSelection();
+	        System.out.println(food);
+	      }
+	      System.out.println("---");
+	    }
+	  }
+	} 
